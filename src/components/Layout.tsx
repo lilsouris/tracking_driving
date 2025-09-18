fef69567fe4072d9390b1d1ea2f1bc01a11b544b
lilsouris <hithomasjay@gmail.com>
2025-09-18 16:13:29 +0200

    Remove unused useAuth import from Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
-import { useAuth } from '../contexts/AuthContext'
 
 interface LayoutProps {
   children: React.ReactNode
